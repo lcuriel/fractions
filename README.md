017d28aa344a43631e01ee7002558c81dd87a9f8
Fer Curiel <lcuriel@itexico.com>
2020-02-13 16:22:05 -0600

    Add MD file

---
+++ README.md
@@ -0,0 +1,16 @@
+#FRACTIONS CALCULATOR
+
+######INTEGER
+	[1-9]\d*\_
+	
+######FRACTION
+	([1-9]\d*\/[1-9]\d*)
+	
+######OPERATOR
+	(\s(\+|\-|\*|\/|)\s)
+######ELEMENT
+	(INTEGER|FRACTION|INTEGERFRACTION)
+	(([1-9]\d*\_)|([1-9]\d*\/[1-9]\d*)|([1-9]\d*\_[1-9]\d*\/[1-9]\d*))
+######ECUATION
+	(INTEGER|FRACTION|INTEGERFRACTION)(OPERATOR(INTEGER|FRACTION|INTEGERFRACTION))*
+	(([1-9]\d*\_)|([1-9]\d*\/[1-9]\d*)|([1-9]\d*\_[1-9]\d*\/[1-9]\d*))(\s(\*|\/|\+|\-)\s(([1-9]\d*\_)|([1-9]\d*\/[1-9]\d*)|([1-9]\d*\_[1-9]\d*\/[1-9]\d*)))*
--- src/fractions.go
+++ src/fractions.go
@@ -1,39 +1,18 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"strconv"
+	"flag"
 )
 
 func main() {
-
-	fooCmd := flag.NewFlagSet("foo", flag.ExitOnError)
-	fooEnable := fooCmd.Bool("enable", false, "enable")
-	fooName := fooCmd.String("name", "", "name")
-
-	barCmd := flag.NewFlagSet("bar", flag.ExitOnError)
-	barLevel := barCmd.Int("level", 0, "level")
-
-	if len(os.Args) < 2 {
-		fmt.Println("expected 'foo' or 'bar' subcommands")
-		os.Exit(1)
+	args := os.Args
+	args = strings.Split(args[1], " ")
+	for i:=0; i< len(args); i++ {
+ 		fmt.Println("VALUE " + strconv.Itoa(i) + " => ", args[i])
 	}
-
-	switch os.Args[1] {
-		case "foo":
-			fooCmd.Parse(os.Args[2:])
-			fmt.Println("subcommand 'foo'")
-			fmt.Println("  enable:", *fooEnable)
-			fmt.Println("  name:", *fooName)
-			fmt.Println("  tail:", fooCmd.Args())
-		case "bar":
-			barCmd.Parse(os.Args[2:])
-			fmt.Println("subcommand 'bar'")
-			fmt.Println("  level:", *barLevel)
-			fmt.Println("  tail:", barCmd.Args())
-		default:
-			fmt.Println("expected 'foo' or 'bar' subcommands")
-			os.Exit(1)
-	}
+	fmt.Println("COMPLETE => ", args)
 }
