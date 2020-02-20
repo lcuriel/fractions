4e602fdd0bafee1776c3aeff48e6edea9ed8c262
Fer Curiel <lcuriel@itexico.com>
2020-02-20 15:16:47 -0600

    Integer to fraction

--- src/operations/basicoperations.go
+++ src/operations/basicoperations.go
@@ -1,30 +1,103 @@
 package operations
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"reflect"
+)
+
 type BasicOperations struct {
 	Numerator int
 	Denominator int
 }
 
-func (bo BasicOperations) multiplication(element1, element2 string) string {
-	return "multiplication"
+func (bo BasicOperations) Multiplication(element1 string, element2 string) string {
+	return "Multiplication"
 }
 
-func (bo BasicOperations) division(element1, element2 string) string {
-	return "division"
+func (bo BasicOperations) Division(element1 string, element2 string) string {
+	return "Division"
 }
 
-func (bo BasicOperations) addition(element1, element2 string) string {
-	return "addition"
+func (bo BasicOperations) Addition(element1 string, element2 string) string {
+	return "Addition"
 }
 
-func (bo BasicOperations) subtraction(element1, element2 string) string {
-	return "subtraction"
+func (bo BasicOperations) Subtraction(element1 string, element2 string) string {
+	return "Subtraction"
 }
 
-func (bo BasicOperations) reduce(element1, element2 string) string {
-	return "reduce"
+func (bo BasicOperations) Reduce(element1 string, element2 string) string {
+	return "Reduce"
 }
 
-func (bo BasicOperations) integerToFraction(element1, element2 string) string {
+func (bo BasicOperations) IntegerToFraction(element string) string {
+	fmt.Println("**************************************************")
+	fmt.Println("**************************************************")
+	fmt.Println("**************************************************")
+	fmt.Println("**************************************************")
+	fmt.Println("**************************************************")
+	fmt.Println("IntegerToFraction -> ORIGINAL: ", element)
+
+	partsFraction := strings.Split(element, "_")
+	if len(partsFraction) == 2 && len(partsFraction[1]) == 0 {
+		partsFraction = []string{partsFraction[0]}
+	}
+	fmt.Println("IntegerToFraction -> partsFraction: ", partsFraction)
+	fmt.Println("IntegerToFraction -> partsFraction -> LEN: ", len(partsFraction))
+
+	// element == 3/4
+	if strings.Contains(partsFraction[0], "/") {
+		fmt.Println("IntegerToFraction -> FRACTION: ", partsFraction[0])
+		return element
+	}
+	// element == 5_
+	if len(partsFraction) == 1 {
+		fmt.Println("IntegerToFraction -> INTEGER: ", partsFraction[0]+"/1")
+		return  partsFraction[0]+"/1"
+	}
+	// element == 5_3/4
+	if len(partsFraction) == 200  {
+		xtem, _ := strconv.Atoi(partsFraction[0])
+		fmt.Println("=============================================")
+		fmt.Println("DATA: ", xtem)
+		fmt.Println("TYPE: ", reflect.TypeOf(xtem))
+		fmt.Println("=============================================")
+		fmt.Println("")
+		fmt.Println("")
+
+
+
+		//return strconv.Itoa( * denominator) + "/2"
+	}
+
+	// element == 5_
+	// element == 5_3/4
+
+
+	/*
+	// [5_], [5_ 3/4]
+
+
+
+
+
+	if !strings.Contains(partsFraction[0], "/") || len(partsFraction) == 2 {
+		integer, _ := strconv.Atoi(partsFraction[0])
+	}
+	// [3/4], [5_ 3/4]
+	if strings.Contains(partsFraction[0], "/") || len(partsFraction) == 2 {
+		element = partsFraction[0] //[3/4]
+		if len(partsFraction) == 2 {
+			element = partsFraction[1] //[5_ 3/4]
+		}
+		partsFraction = strings.Split(element, "/")
+		numerator, _ := strconv.Atoi(partsFraction[0])
+		denominator, _ := strconv.Atoi(partsFraction[1])
+	}
+*/
+
+
 	return "integerToFraction"
 }
--- src/operations/fractionsoperations.go
+++ src/operations/fractionsoperations.go
@@ -9,8 +9,9 @@ import (
 
 
 type FractionsOperations struct {
-	Result string
-	Tracking []string
+	result string
+	tracking []string
+	elements []string
 }
 
 func (f *FractionsOperations) IsValid(operation string) bool {
@@ -38,38 +39,53 @@ func (f FractionsOperations) CleanString(operation string) string{
 }
 
 func (f FractionsOperations) Execute(operation string) string{
-	elements := strings.Split(operation, " ")
-	fmt.Println("ELEMENTS: ", elements)
+	result := ""
+	f.elements = strings.Split(operation, " ")
+
 	operators := [4]string{"*", "/", "+", "-"}
 	for i:=0; i< len(operators); i++{
-		fmt.Println("ELEMENT: ", operators[i])
+		/*
+		fmt.Println("OPERATOR: ", operators[i])
+		fmt.Println("=========================")
+		 */
 		for {
-			indexOperator := f.indexOf(elements, operators[i])
-			element1, element2 := f.getElements(indexOperator)
-
-			fmt.Println("INDEX: ", indexOperator)
-			fmt.Println("==========")
+			indexOperator := f.indexOf(f.elements, operators[i])
+			if indexOperator >= 0 {
+				element1, element2 := f.getElements(indexOperator)
+				result = f.doOperation(operators[i], element1, element2)
+				f.newArray(f.elements, result)
+			}
 			break
 		}
+		i = len(operators)
 	}
-	/* */
 	return "execute"
 }
 
-func (f FractionsOperations) DoOperation(operation string) string {
-	return "doOperation"
+func (f FractionsOperations) getElements(indexOperator int) (string, string) {
+	fmt.Println("ELEMENTS: ", f.elements)
+	fmt.Println("indexOperator: ", indexOperator)
+	e1 := f.elements[indexOperator-1]
+	e2 := f.elements[indexOperator+1]
+	return e1, e2
 }
 
-func (f FractionsOperations) DisplayResult(operation string) string {
-	return "displayResult"
-}
-
-func (f FractionsOperations) SeachOperator(operation string) int {
-	return 666
-}
-
-func (f FractionsOperations) NewArray(operation string) string {
-	return "newArray"
+func (f FractionsOperations) doOperation(operator string, element1 string, element2 string) string {
+	bo := new(BasicOperations)
+	var result string
+	element1 = bo.IntegerToFraction(element1)
+	element2 = bo.IntegerToFraction(element2)
+	switch operator{
+		case "*":
+			result = bo.Multiplication(element1, element2)
+		case "/":
+			result = bo.Division(element1, element2)
+		case "+":
+			result = bo.Addition(element1, element2)
+		case "-":
+			result = bo.Subtraction(element1, element2)
+	}
+	return result
 }
 
 func (f FractionsOperations) indexOf(vs []string, t string) int {
@@ -80,10 +96,20 @@ func (f FractionsOperations) indexOf(vs []string, t string) int {
 	}
 	return -1
 }
-func (f FractionsOperations) getElements(index int) (string, string) {
-	
-	return "element01", "element02"
+/*
+ ===================================================================================================
+ ===================================================================================================
+ ===================================================================================================
+ ===================================================================================================
+ */
+func (f FractionsOperations) newArray(operation []string, result string) string {
+	return "newArray"
 }
+
+func (f FractionsOperations) DisplayResult(operation string) string {
+	return "displayResult"
+}
+
 
 
 
